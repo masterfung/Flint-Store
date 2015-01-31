4c7ee4751e74ae2cd64842b592140036a975d708
Tsung Hung <hungmasterj@gmail.com>
2015-01-30 16:40:23 -0800

    added to about, product page, and images

--- js/app.js
+++ js/app.js
@@ -1,11 +1,50 @@
-App = Ember.Application.create();
-
-App.Router.map(function() {
-  // put your routes here
+Flint = Ember.Application.create({
+	LOG_TRANSITIONS: true
 });
 
-App.IndexRoute = Ember.Route.extend({
+Flint.Router.map(function() {
+  this.route('about');
+  this.resource('products');
+});
+
+Flint.IndexRoute = Ember.Route.extend({
   model: function() {
     return ['red', 'yellow', 'blue'];
   }
+});
+
+Flint.IndexController = Ember.Controller.extend({
+	productType: 2,
+	rock_orange: 'img/flint.png',
+	current_time: function () {
+		return(new Date()).toDateString()
+	}.property()
+});
+
+var products = [
+	{
+		title: 'Flint',
+		price: 29,
+		description: "Awesome for simple building and our most affordable material" +
+		"yet",
+		isOnSale: true,
+		image: 'img/flint.png'
+	},
+	{
+		title: 'Orine',
+		price: 999,
+		description: "One of the finest materials on Planet Flint. We cherish great" +
+		"build materials and we know Orine will never disappoint.",
+		isOnSale: true,
+		image: 'img/orine.png'
+	}];
+
+Flint.ProductsRoute = Ember.Route.extend({
+	model: function () {
+		return products;
+	}
+});
+
+Flint.ProductController = Ember.Controller.extend({
+
 });
